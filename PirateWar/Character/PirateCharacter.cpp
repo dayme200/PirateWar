9798f7b56ab75351c8b26ca6cc239bd4e9f8e82e
dayme200 <dayme200@gmail.com>
2024-05-27 17:54:07 +0900

    Feat: PirateCharacter Bind Default Input

--- PirateWar/Character/PirateCharacter.cpp
+++ PirateWar/Character/PirateCharacter.cpp
@@ -1,9 +1,19 @@
 #include "PirateCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 APirateCharacter::APirateCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh());
+	CameraBoom->TargetArmLength = 600.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
 }
 
 void APirateCharacter::BeginPlay()
@@ -19,5 +29,41 @@ void APirateCharacter::Tick(float DeltaTime)
 void APirateCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &APirateCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APirateCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &APirateCharacter::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &APirateCharacter::LookUp);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 }
 
+void APirateCharacter::MoveForward(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void APirateCharacter::MoveRight(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void APirateCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void APirateCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
--- PirateWar/Character/PirateCharacter.h
+++ PirateWar/Character/PirateCharacter.h
@@ -17,6 +17,15 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-private:	
 
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
+	
+private:
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class UCameraComponent* FollowCamera;
 };
