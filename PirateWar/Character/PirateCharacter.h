555020896a1b6a911d099bd10f2ff5350326324e
dayme200 <dayme200@gmail.com>
2024-06-11 14:22:46 +0900

    Init: Hit Boxes server-side rewind

--- PirateWar/Character/PirateCharacter.h
+++ PirateWar/Character/PirateCharacter.h
@@ -39,6 +39,48 @@ public:
 	void ShowSniperScopeWidget(bool bShowScope);
 
 	void SpawnDefaultWeapon();
+
+	/*
+	 * Hit boxes used for server-side rewind
+	 */
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* head;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* pelvis;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* spine_01;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* spine_02;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* spine_03;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* spine_04;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* spine_05;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* upperarm_l;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* upperarm_r;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* lowerarm_l;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* lowerarm_r;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* hand_l;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* hand_r;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* thigh_l;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* thigh_r;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* calf_l;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* calf_r;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* foot_l;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* foot_r;
 	
 protected:
 	virtual void BeginPlay() override;
